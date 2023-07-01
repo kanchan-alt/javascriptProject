98fcbbf36e48211af7047881b0be8253342e13e1
kanchanlatakujur <kanchanlatakujur41@gmail.com>
2023-07-01 22:03:28 +0530

    new way of todolist

---
+++ todolist/script.js
@@ -0,0 +1,89 @@
+const listsContainer = document.querySelector('[data-lists]')
+// console.log(listsContainer);
+
+const newListForm = document.querySelector('[data-new-list-form]');
+const newListInput = document.querySelector('[data-new-list-input]')
+
+const deleteListButton = document.querySelector('[data-delete-list-button]')
+
+
+const LOCAL_STORAGE_LIST_KEY = 'task.lists'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY))|| []
+
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
+})
+
+
+listsContainer.addEventListener('click', e => {
+  if(e.target.tagName.toLowerCase() === 'li'){
+    selectedListId = e.target.dataset.listId
+    saveAndRender()
+  }
+})
+
+
+newListForm.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const listName = newListInput.value
+    if (listName == null || listName === '') return
+
+    const list = createList(listName)
+
+    newListInput.value = null
+    lists.push(list)
+    // render()
+    saveAndRender()
+})
+
+
+function createList(name){
+   return { id: Date.now().toString(), name : name, task : []}
+}
+
+function saveAndRender(){
+    save()
+    render()
+}
+
+function save(){
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
+}
+
+
+function render() {
+    // <li class="list-name">Work</li>
+    clearElement(listsContainer)
+    lists.forEach(list => {
+      const listElement = document.createElement('li')
+      listElement.dataset.listId = list.id
+      listElement.classList.add("list-name")
+      listElement.innerText = list.name
+      if(list.id === selectedListId){
+        listElement.classList.add('active-list')
+      }
+      listsContainer.appendChild(listElement)
+    })
+}
+
+
+function clearElement(element) {
+     
+    while(element.firstChild){
+    
+        element.removeChild(element.firstChild)
+    }
+
+}
+
+render()
+
