6894e3bed4f4168522d3a3c981021e66a06688b0
kanchanlatakujur <kanchanlatakujur41@gmail.com>
2023-07-12 23:21:49 +0530

    know for in loop basic

---
+++ arrayAndObjectMethods/object/object.js
@@ -0,0 +1,50 @@
+// let user = new Object(); // "object constructor" syntax
+
+
+// let user1 = {};  // "object literal" syntax
+
+// let user2 = {
+//     name: "John",
+//     age: 30,
+//     "likes birds": true  // multiword property name must be quoted
+//   };
+
+let user = {
+    name: "John",
+    age: 30
+  };
+
+  
+  // access by variable
+//   console.log(user["name"]); // John (if enter "name")
+//   console.log(user.name);
+
+
+//   function makeUser(name, age) {
+//     return {
+//       name: name,//name
+//       age: age,//age
+//       // ...other properties
+//     };
+//   }
+  
+//   let user = makeUser("John", 30);
+//   console.log(user.name); // John
+
+/**
+ * how for loop for object
+ */
+for (let value in user) {
+    // keys
+    console.log( value );  // name, age
+    // values for the keys
+   
+}
+
+
+  for (let value in user) {
+    // keys
+    console.log( user[value] );  // john, 30
+    // values for the keys
+   
+  }
