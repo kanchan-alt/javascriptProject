b3f183c5efc4f424246bb7feda178ac3646b765a
kanchanlatakujur <kanchanlatakujur41@gmail.com>
2023-07-19 23:21:15 +0530

    4program done outof 35

---
+++ code/varible.js
@@ -0,0 +1,22 @@
+
+let varible = "hi i am string"
+let boolean = true;
+let NULL = null;
+let UndeFined = undefined;
+
+// console.log(`${varible} varble of type ${typeof varible}`);
+// console.log(`${boolean} varible of type ${typeof boolean}`);
+// console.log(`${NULL} varible of type ${typeof NULL}`);
+// console.log(`${UndeFined} varible of type  ${typeof UndeFined}`);
+
+
+let firstName = "blake";
+let lastName = "Ryan";
+let status =  "married";
+let age = 30;
+
+// console.log(`my name ${firstName} ${lastName} i am ${age} old ${status}`)
+
+
+let statement = "You can't be paralyzed by fear of failure or you will never push yourself."
+// console.log(statement.toUpperCase())
