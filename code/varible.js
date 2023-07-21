2e23770be617bd1123d71315966ff1a84c7033be
kanchanlatakujur <kanchanlatakujur41@gmail.com>
2023-07-21 16:01:10 +0530

    11/35 done

--- code/varible.js
+++ code/varible.js
@@ -43,10 +43,73 @@ let lastOccurrenceIndex = text.lastIndexOf("script");
 
 let findOccuuenceBeacause = 'You cannot end a sentence with because because because is a conjunction'
 let firstOccurrenceIndex = findOccuuenceBeacause.indexOf("because");
-console.log(`find Occuuence Word  because is at index ${firstOccurrenceIndex}`)
+// console.log(`find Occuuence Word  because is at index ${firstOccurrenceIndex}`)
 
 
 let beforeTrim = "   Do something today that your future self will thank you for.   "
 let afterTrim = beforeTrim.trim()
- console.log( beforeTrim)
- console.log(afterTrim)
+//  console.log( beforeTrim)
+//  console.log(afterTrim)
+
+
+ //10
+
+
+ - 4 > 3
+   // -4 is indeed less than 3, so the expression is false.
+    console.log(-4 > 3);//false
+
+- 4 >= 3
+// -4 is indeed less than equal than  3, so the expression is false.
+    console.log(-4 >= 3);//false
+
+- 4 < 3
+// -4 is indeed less than  than  3, so the expression is true.
+console.log(-4 < 3);//true
+
+- 4 <= 3
+// -4 is indeed less than equal than  3, so the expression is true.
+console.log(-4 <= 3);//true
+
+- 4 == 4
+// they are different numbers with different numerical values. so the expression is false.
+console.log(-4 == 4);//false
+
+- 4 === 4
+// In the case of "-4 === 4", the statement is false. The value of -4 is not equal to 4, and their types are also different (one is a negative integer, and the other is a positive integer). So, the strict equality comparison returns false..
+console.log(-4 === 4);//false
+
+
+- 4 != 4
+// The statement "-4 != 4" is true. The "!=" operator checks for inequality, and it returns true when the two values being compared are not equal. In this case, -4 is not equal to 4, so the statement is true..
+console.log(-4 != 4);//true
+
+
+- 4 !== 4
+// The statement "4 !== 4" is false. The "!=="" operator is used for strict inequality comparison, which means it checks both the values and types of the operands. In this case, both operands are the number 4 and -4, and they have the diff value and type (both are numbers), so the strict inequality comparison returns true
+
+console.log(-4 !== 4);//true
+
+
+- 4 != '4'
+// -4 is not equal to string '4', so the expression is true.
+console.log(-4 != 4);//true
+
+- 4 == '4'
+// -4  and  4 are total different value, so the expression is false.
+console.log(-4 == 4);//false
+
+
+- 4 === '4'
+// -4 and 4 are total different value and type are number(both) bez of strict inequality comparison, so the expression is false.
+console.log(-4 === 4);//false
+
+// Find the length of python and jargon and make a falsy comparison statement.
+
+let python ="python"
+console.log(python.length);
+let jargon ="jargon"
+console.log(python.length);
+ python === jargon
+
+
